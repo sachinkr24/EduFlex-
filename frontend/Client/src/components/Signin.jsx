59aa8c72e2edff7860f599af5d1dad7c79a90e41
kapil000001 <97395446+kapil000001@users.noreply.github.com>
2024-06-11 20:53:09 +0530

    resolved

--- frontend/Client/src/components/Signin.jsx
+++ frontend/Client/src/components/Signin.jsx
@@ -8,12 +8,8 @@ import Appbar from './Appbar.jsx';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { signIn } from '../Logics/Signin.js';
-<<<<<<< HEAD
-import { useNavigate } from 'react-router-dom';
 import { z } from 'zod';
-=======
 import axios from 'axios';
->>>>>>> c8c204031bb1b77964ce0b85179d38518143af1e
 
 export default function Signup() {
   const [email, setEmail] = useState('');
@@ -21,17 +17,12 @@ export default function Signup() {
   const [alignment, setAlignment] = useState('USER');
   const [reset, setReset] = useState(false);
   const [newPassword, setNewPassword] = useState('');
+  const [errors, setErrors] = useState({});
 
-<<<<<<< HEAD
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [alignment, setAlignment] = useState('USER');
-    const [errors, setErrors] = useState({});
-=======
+
   const handleChange = (e, newAlignment) => {
     setAlignment(newAlignment);
   };
->>>>>>> c8c204031bb1b77964ce0b85179d38518143af1e
 
   const resetPass = () => {
     setReset(true);
@@ -39,7 +30,6 @@ export default function Signup() {
 
   const navigate = useNavigate();
 
-<<<<<<< HEAD
     // Define the Zod schema here
     const authSchema = z.object({
         email: z.string().email({ message: "Invalid email address" }),
@@ -66,74 +56,7 @@ export default function Signup() {
         }
     };
 
-    return <div>
-        <Appbar />
-        <div style={{
-            paddingTop: 150,
-            marginBottom: '10',
-            display: 'flex',
-            justifyContent: 'center',
-        }}>
-            <Typography variant="h6">
-                Welcome Back !! Signin Below
-            </Typography>
-        </div>
-        <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-        }}>
-            <Card variant='outlined' style={{
-                padding: '10px',
-                width: '400px',
-            }}>
-                <div style={{
-                    marginBottom: '10px',
-                    display: 'flex',
-                    justifyContent: 'center',
-                }}>
-                    <ToggleButtonGroup
-                        color="primary"
-                        exclusive
-                        value={alignment}
-                        aria-label="Platform"
-                        onChange={handleChange}
-                    >
-                        <ToggleButton value="USER">USER</ToggleButton>
-                        <ToggleButton value="ADMIN">ADMIN</ToggleButton>
-                    </ToggleButtonGroup>
-                </div>
-                <TextField
-                    label="Email"
-                    variant="outlined"
-                    style={{
-                        width: "100%",
-                        marginBottom: '10px',
-                    }}
-                    onChange={(e) => setEmail(e.target.value)}
-                    error={Boolean(errors.email)}
-                    helperText={errors.email}
-                />
-                <TextField
-                    type='password'
-                    label="Password"
-                    variant="outlined"
-                    style={{
-                        width: "100%",
-                        marginBottom: '10px',
-                    }}
-                    onChange={(e) => setPassword(e.target.value)}
-                    error={Boolean(errors.password)}
-                    helperText={errors.password}
-                />
-                <Button
-                    size={'large'}
-                    variant='contained'
-                    onClick={handleSubmit}
-                >
-                    signin
-                </Button>
-            </Card>
-=======
+    
   const changePassword = async (email, newPassword, role) => {
       var sendTo = 'admin';
       if(role == 'USER') sendTo = 'users';
@@ -220,7 +143,6 @@ export default function Signup() {
               Reset Password
             </Button>
           </Card>
->>>>>>> c8c204031bb1b77964ce0b85179d38518143af1e
         </div>
       ) : (
         <>
@@ -318,8 +240,5 @@ export default function Signup() {
         </>
       )}
     </div>
-<<<<<<< HEAD
-=======
   );
->>>>>>> c8c204031bb1b77964ce0b85179d38518143af1e
 }
